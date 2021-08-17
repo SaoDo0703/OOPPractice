e7977440ab1eee35776980bd19a1e2e769577b0f
SaoDo <Dothisao070322@gmail.com>
2021-08-17 20:44:38 +0700

    first commit

---
+++ src/com/javacoban/oop/lesson7/Dog.java
@@ -0,0 +1,20 @@
+package com.javacoban.oop.lesson7;
+
+public class Dog {
+    public static void main(String[] args) {
+       String age;
+       String color;
+
+    }
+
+    void eatMeal(){
+
+    }
+
+    void goOut(){
+
+    }
+
+
+}
+
